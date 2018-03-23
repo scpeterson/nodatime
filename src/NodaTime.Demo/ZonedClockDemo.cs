b4787fdc1ace34a886e3e2b897f1067e14749b24
scpeterson <tortilla.curtain@gmail.com>
2018-03-23 14:32:59 -0400

    Issue 918 - ZonedClock snippet

---
+++ src/NodaTime.Demo/ZonedClockDemo.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace NodaTime.Demo
+{
+    public class ZonedClockDemo
+    {
+        [Test]
+        public void GetCurrentDate()
+        {
+            //ZonedClock zonedClock = Snippet.For(ZonedClock.)
+        }
+    }
+}
